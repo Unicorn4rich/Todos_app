b7c87a71a8d295e62ccbe6bbceed916fa7f80f4e
Rimi Works <sk9952908@gmail.com>
2024-04-01 15:03:53 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,33 @@
+// let fruitchaat: string[] = ["Banana", "Apple", "Orrange"];
+
+// fruitchaat.push("Milk")
+// ye function array ke end mein aik new value add krwata hai.
+
+
+// fruitchaat.pop();
+// ye function array ki end wali value ko delete kar ke usy apne andar store kar leta hai..
+
+
+// fruitchaat = fruitchaat.concat(["Water mellon", "Yougart", "Siwayyan", "Cream"]);
+// is function se aik array ki list dosry array mein daal sakty hain...
+
+// console.log(fruitchaat);
+
+
+
+// while loop
+let i = 0;
+
+while(i <= 10){
+     // console.log(i)    // example 1
+    console.log("Fasting", "Sehri", "Aftari"); // example 2
+    i++;
+}
+
+// is loop se ham kisi bhi value ki limit set kar ke usko multiple time run kar sakty hain.
+// jese i ki value 0 dey kasr uski limit 10 kardi hai ab ye loop 0 index se ly kar 10 tak chalega
+// uske bad jese hi 10 ki value tak jaega wahn par stop ho jaega...
+
+//  i++;
+// or ye jo i ko incriment krwaty hain agr ye nahi krwaenge to loop hmesha 0 se 0 tak hi chalta rhega
+// or kabhi stop nahi hoga.
---
+++ todo.js
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let store = [];
+let condition = true;
+while (condition) {
+    let todoApp = await inquirer.prompt([
+        {
+            name: "add",
+            message: "What you want to add in your todos?",
+            type: "input"
+        },
+        {
+            name: "addmore",
+            message: "Do you want to add more ?",
+            type: "confirm",
+            default: "false"
+        }
+    ]);
+    store.push(todoApp.add);
+    condition = todoApp.addmore;
+    console.log(store);
+}
+// NOTES..
+//   name: "add",
+// is type se ye hoga ke ham jo bhi isme input denge ye avail kar lega apne pass num ho ya string.
+// type: "confirm"
+// is type se ye hoga inquirer hamse jo bhi input lega ya to wo Yes mein hoga ya No mein
+// or jab bhi ham (type: "confirm") ye type use karte hain tab hame (default: "false") ye lazim lgana parta hai.
+// while loop
+// agr ham chahty hain aik hi question bar bar pocha jaye to uske liye hain while loop ka istmal karenge.
